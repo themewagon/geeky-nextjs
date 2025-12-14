f8dcb1ae8908e4793f8ff2277c80a8f8d6c66cef
KhanAnupamShafi <deep71.bd@gmail.com>
2025-12-14 14:24:29 +0600

    first commit

---
+++ next.config.js
@@ -0,0 +1,22 @@
+
+const isProd = process.env.NODE_ENV === "production";
+
+const basePath = isProd ? "/geeky-nextjs" : "";
+
+const nextConfig = {
+  output: "export",
+  basePath,
+  assetPrefix: basePath,
+  images: {
+    unoptimized: true,
+  },
+  trailingSlash: true,
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  env: {
+    NEXT_PUBLIC_BASE_PATH: basePath,
+  },
+};
+
+export default nextConfig;
